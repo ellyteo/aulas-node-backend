5f66193a38fe8e4ddd994785bc5f9b5040312cca
Gabriélly Teodoro <gabitsilva1204@gmail.com>
2026-08-06 22:11:39 -0300

    criação do zero e rotas de criação, delete e buscas

---
+++ router/carro.js
@@ -0,0 +1,52 @@
+import express from 'express'
+import carros from '../repository/carro.js'
+
+const router = express.Router()
+
+// Buscar todos
+router.get("/buscar", (req, res) => {
+    res.send({ message: carros })
+})
+
+// Buscar um
+router.get("/detalhe/:id", (req, res) => {
+    const id = req.params.id
+
+    const carro = carros.find(it => it.id === id)
+
+    if (!id ) {
+        res.send({ message: "Carro não encontrado!" })
+        return
+    }
+    res.send({ message: carro })
+
+})
+
+// Criar
+router.post("/criar", (req, res) => {
+    const { id, marca, ano } = req.body
+
+    if (!id || !marca || !ano) {
+        res.send({ message: "Favor informar todos os dados!" })
+        return
+    }
+
+    carros.push({ id, marca, ano })
+    res.send({ message: "Cadastro com sucesso" })
+})
+
+// Alterar
+router.post("/alterar", (req, res) => {
+
+})
+
+// Deletar
+router.post("deletar", (req, res) => {
+    const id = req.body.id
+
+    carros.splice(it => it.id === id, 1)
+
+    res.send({ message: "Deletado!"})
+})
+
+export default router
